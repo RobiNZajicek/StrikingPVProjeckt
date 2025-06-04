28405c17a1037961c3e02211df69c989b0e8d9e5
RobiNZajicek <zajicekrobin3@gmail.com>
2025-06-05 00:15:22 +0200

    hope3

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://pv-projekt.vercel.app/', // nebo vlastní doména
+    siteUrl: 'https://pv-striking.vercel.app/', // nebo vlastní doména
     generateRobotsTxt: true,
   };
   
